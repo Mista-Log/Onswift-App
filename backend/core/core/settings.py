01130b8be7ecd3a045865729e80ae6188bbac0b6
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-01-28 16:33:34 +0100

    Remove hardcoded FRONTEND_URL from settings and load from environment variable

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -199,4 +199,5 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
-FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:8080/")
+FRONTEND_URL = os.environ.get("FRONTEND_URL")
+
