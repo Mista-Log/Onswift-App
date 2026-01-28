50c2f497550a39e1529dee46d8d788e215323d0c
Mista-Log <oloyedeibrahimsmile@gmail.com>
2026-01-28 19:06:55 +0100

    ...

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -207,8 +207,8 @@ SIMPLE_JWT = {
 }
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
